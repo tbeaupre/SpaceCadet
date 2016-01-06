378a3ee76e1cdf2fc8f112e23b6c17fc52569bb5
Tyler Beaupre <tylerbeaupre4@gmail.com>
2016-01-06 18:31:27 -0500

    Added IProjectileData Interface

---
+++ WindowsGame1/WindowsGame1/IProjectileData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spaceman
+{
+    interface IProjectileData
+    {
+        Projectile CreateProjectile();
+        void UpdateProjectile();
+        void DestroyProjectile();
+    }
+}
